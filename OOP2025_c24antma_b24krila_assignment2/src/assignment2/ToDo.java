2a6bf9d2a0b8c56524fdfb899283818bfe346658
Amark558 <amarklund262@gmail.com>
2025-02-13 15:16:26 +0100

    Add files via upload

---
+++ OOP2025_c24antma_b24krila_assignment2/src/assignment2/ToDo.java
@@ -0,0 +1,23 @@
+package assignment2;
+import javax.swing.JFrame;
+import se.his.it401g.todo.*;
+
+public class ToDo {
+	
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		
+		Task task = new HomeTask();
+		Task taskStudy = new StudyTask();
+		
+		frame.add(taskStudy.getGuiComponent());
+		frame.add(task.getGuiComponent());
+		
+		
+		frame.setBounds(100, 100, 400, 400);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
